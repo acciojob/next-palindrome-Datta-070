271d49cff8ed084a15e3d8ea2066cc02ddfb0bbc
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-05-16 16:37:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function nextPalindrome(num) {
   //your JS code here.
+	function isPalindrome(num) {
+        return num.toString() === num.toString().split('').reverse().join('');
+    }
+	while (true) {
+        num++;
+        if (isPalindrome(num)) {
+            return num;
+        }
+    }
 }
 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+if (!isNaN(input) && input > 0 && input <= 100000) {
+    // Find next palindrome
+    const result = nextPalindrome(input);
+    // Display the result via alert
+    alert("The next palindrome number is: " + result);
+} else {
+    // Display error message if input is invalid
+    alert("Invalid input. Please enter a positive integer up to 100000.");
+}
