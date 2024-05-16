91a9b3a7b1dd2a7c53701f84b67611f4ddb6cc59
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-05-16 16:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ if (!isNaN(input) && input > 0 && input <= 100000) {
     // Find next palindrome
     const result = nextPalindrome(input);
     // Display the result via alert
-    alert("The next palindrome number is: " + result);
+    alert(result);
 } else {
     // Display error message if input is invalid
     alert("Invalid input. Please enter a positive integer up to 100000.");
